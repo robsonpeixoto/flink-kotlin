946e37c27f7e69f72218e84692f03989ed65fe31
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2023-03-15 11:10:24 -0300

    refactor: using array to define a collection of headers

--- src/main/kotlin/dev/robsonpeixoto/pojo/Event.kt
+++ src/main/kotlin/dev/robsonpeixoto/pojo/Event.kt
@@ -2,6 +2,6 @@ package dev.robsonpeixoto.pojo
 
 
 sealed interface Event
-data class Headers(var contentType: String = "")
-sealed class CloudEvent(var eventType: String, var headers: Headers = Headers(contentType = "json")) : Event
+data class Header(var key: String = "", var value: String = "")
+sealed class CloudEvent(var eventType: String, var headers: Array<Header> = emptyArray()) : Event
 data class GithubEvent(var numPR: Int = 0) : CloudEvent(eventType = "github")
